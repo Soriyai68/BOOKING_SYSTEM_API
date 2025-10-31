bcf6bea2ac42020c62e109362b3cafe7faa101a9
HengHoursan <157660651+HengHoursan@users.noreply.github.com>
2025-10-31 14:47:48 +0700

    updated permission

--- routes/permission.routes.js
+++ routes/permission.routes.js
@@ -3,18 +3,17 @@ const router = express.Router();
 const authenticate = require('../middlewares/auth.middleware');
 const authorize = require('../middlewares/authorize.middleware');
 const validator = require('../middlewares/validator.middleware');
-const { getAllPermissionsQuerySchema } = require('../schemas/permissionSchema');
-const { requirePermission, PERMISSIONS } = require('../middlewares/permission.middleware');
-const { Role } = require('../data');
+const {requirePermission, PERMISSIONS} = require('../middlewares/permission.middleware');
+const {Role} = require('../data');
 const {
-  getUserPermissions,
-  checkPermission,
-  getAllPermissions,
-  getRolePermissions,
-  getAllRolePermissions,
-  createPermission,
-  assignPermissionToRole,
-  removePermissionFromRole
+    getUserPermissions,
+    checkPermission,
+    getAllPermissions,
+    getRolePermissions,
+    getAllRolePermissions,
+    createPermission,
+    assignPermissionToRole,
+    removePermissionFromRole
 } = require('../controllers/permission.controller');
 
 // All routes require authentication
@@ -27,36 +26,35 @@ router.get('/me', getUserPermissions);
 router.post('/check', checkPermission);
 
 // Admin and SuperAdmin only routes
-router.get('/all', 
-  authorize(Role.ADMIN, Role.SUPERADMIN), 
-  validator(getAllPermissionsQuerySchema, 'query'),
-  getAllPermissions
+router.get('/all',
+    authorize(Role.ADMIN, Role.SUPERADMIN),
+    getAllPermissions
 );
 
-router.get('/roles', 
-  authorize(Role.ADMIN, Role.SUPERADMIN), 
-  getAllRolePermissions
+router.get('/roles',
+    authorize(Role.ADMIN, Role.SUPERADMIN),
+    getAllRolePermissions
 );
 
-router.get('/roles/:role', 
-  authorize(Role.ADMIN, Role.SUPERADMIN), 
-  getRolePermissions
+router.get('/roles/:role',
+    authorize(Role.ADMIN, Role.SUPERADMIN),
+    getRolePermissions
 );
 
 // SuperAdmin only routes
-router.post('/', 
-  authorize(Role.SUPERADMIN), 
-  createPermission
+router.post('/',
+    authorize(Role.SUPERADMIN),
+    createPermission
 );
 
-router.post('/assign', 
-  authorize(Role.SUPERADMIN), 
-  assignPermissionToRole
+router.post('/assign',
+    authorize(Role.SUPERADMIN),
+    assignPermissionToRole
 );
 
-router.post('/remove', 
-  authorize(Role.SUPERADMIN), 
-  removePermissionFromRole
+router.post('/remove',
+    authorize(Role.SUPERADMIN),
+    removePermissionFromRole
 );
 
 module.exports = router;
