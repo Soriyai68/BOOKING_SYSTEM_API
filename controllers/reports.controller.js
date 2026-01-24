4f88ee7549e6624666d5df892045ed0d9156b40c
Soriya11 <soreya414243@gmail.com>
2026-01-25 00:44:13 +0700

    updated_report

--- controllers/reports.controller.js
+++ controllers/reports.controller.js
@@ -12,6 +12,45 @@ exports.getTotalCustomers = async (req, res) => {
  }
 }
 
+//report total bookings
+exports.getTotalBookings = async (req, res) => {
+ try {
+  const totalBookings = await reports.Booking.countDocuments();
+  res.status(200).json({ totalBookings });
+ } catch (error) {
+  res.status(500).json({ message: error.message });
+ }
+}
+
+//report total revenue
+exports.getTotalRevenue = async (req, res) => {
+ try {
+  const totalRevenue = await reports.Payment.aggregate([
+   {
+    $group: {
+     _id: null,
+      totalRevenue: { $sum: "$amount" }
+    }
+   }
+  ]);
+  res.status(200).json({ totalRevenue: totalRevenue[0].totalRevenue });
+ } catch (error) {
+  res.status(500).json({ message: error.message });
+ }
+}
+
+//total movies
+exports.getTotalMovies = async (req, res) => {
+ try {
+  const totalMovies = await reports.Movie.countDocuments();
+  res.status(200).json({ totalMovies });
+ } catch (error) {
+  res.status(500).json({ message: error.message });
+ }
+}
+
+
+
 
 
 
--- routes/reports.routes.js
+++ routes/reports.routes.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const { Role } = require('../data');
-const middlewares = require('../middlewares');
-const ReportsController = require('../controllers/reports.controller');
-
 const router = express.Router();
+const reportController = require('../controllers/reports.controller');
 
-// GET /api/v1/reports/total-customers - Get total number of customers
-router.get('/total-customers',
- middlewares.authenticate,
- middlewares.authorize(Role.ADMIN, Role.SUPERADMIN),
- ReportsController.getTotalCustomers
-);
+router.get('/total-customers', reportController.getTotalCustomers);
+router.get('/total-bookings', reportController.getTotalBookings);
+router.get('/total-revenue', reportController.getTotalRevenue);
+router.get('/total-movies', reportController.getTotalMovies);
 
 module.exports = router;
